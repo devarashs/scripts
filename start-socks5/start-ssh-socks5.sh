12f7825c82a43ce8e51c27429fb6b1d502ed5216
devarashs <southernwavesofmadness@protonmail.com>
2024-01-13 22:58:07 +0330

    adjust:start-ssh-socks5

--- start-socks5/start-ssh-socks5.sh
+++ start-socks5/start-ssh-socks5.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# Function to get user input for port, user, host, and sleep duration
+# Function to get user input for port, user, and host
 get_user_input() {
     read -p "Enter the port number: " port
     read -p "Enter the user name: " user
     read -p "Enter the host name: " host
-    read -p "Enter the sleep duration (in seconds): " sleep_duration
-}
-
-# Function to start the SSH command
-start_ssh_command() {
-    while true; do
-        ssh -D ${ssh_settings[0]} -q -C -N -f ${ssh_settings[1]}@${ssh_settings[2]}
-        sleep $sleep_duration
-    done
 }
 
 # Check if the variables have already been set
@@ -30,7 +21,7 @@ if [ -f ".ssh_settings" ]; then
     echo "User: $user"
     echo "Host: $host"
 
-    # Ask user if they want to use saved values or enter new ones
+    # Ask the user if they want to use saved values or enter new ones
     read -p "Do you want to use these saved values? (y/n): " use_saved
     if [ "$use_saved" == "n" ]; then
         get_user_input
@@ -45,5 +36,5 @@ echo "port=${ssh_settings[0]}" > .ssh_settings
 echo "user=${ssh_settings[1]}" >> .ssh_settings
 echo "host=${ssh_settings[2]}" >> .ssh_settings
 
-# Start the SSH command in the background
-start_ssh_command &
+# Start the SSH command using autossh in the background
+ssh -D ${ssh_settings[0]} -q -C -N -f ${ssh_settings[1]}@${ssh_settings[2]}
